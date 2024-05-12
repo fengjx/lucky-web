cb3c0ffacfb8df703d6f49109bbff57ed1cf24de
fengjx <fengjianxin2012@gmail.com>
2024-05-12 15:18:54 +0800

    env

--- public/env.js
+++ public/env.js
@@ -1,5 +1,11 @@
 const envList = [
   {
+    label: '开发环境',
+    value: 'dev',
+    apiBaseUrl: 'http://localhost:8080',
+    schemaBaseUrl: 'http://localhost:8080/static/pages',
+  },
+  {
     label: 'demo环境',
     value: 'demo',
     apiBaseUrl: 'http://8.134.143.210:8080',
@@ -10,4 +16,4 @@ const envList = [
 console.log('envList', envList)
 
 window.envList = envList
-window.defaultEnv = 'demo'
+window.defaultEnv = 'dev'
