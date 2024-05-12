f4222fa11df915bc31b816e0f72323ca15ca5790
fengjx <fengjianxin2012@gmail.com>
2024-05-12 14:16:37 +0800

    deno env

--- public/env-demo.js
+++ public/env-demo.js
@@ -1,11 +1,5 @@
 const envList = [
   {
-    label: '开发环境',
-    value: 'dev',
-    apiBaseUrl: 'http://localhost:8080',
-    schemaBaseUrl: 'http://localhost:8080/static/pages',
-  },
-  {
     label: 'demo环境',
     value: 'demo',
     apiBaseUrl: 'http://8.134.143.210:8080',
